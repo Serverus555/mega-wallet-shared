f1ae0c65ed10db44d4e86f64d54a3431d3df97b4
serverus <serverus555@yandex.ru>
2026-07-24 13:57:48 +0700

    Events

---
+++ event/transaction.go
@@ -0,0 +1,49 @@
+package event
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Envelope struct {
+	Type  string
+	Event json.RawMessage
+}
+
+type baseTransactionEvent struct {
+	TraceId string
+	UserId  uuid.UUID
+	Amount  string
+
+	Start time.Time
+	End   time.Time
+	Err   *string
+}
+
+type DepositEvent struct {
+	baseTransactionEvent
+	Currency string
+	Amount   string
+}
+
+type WithdrawEvent struct {
+	baseTransactionEvent
+	Currency string
+	Amount   string
+}
+
+type ExchangeEvent struct {
+	baseTransactionEvent
+	FromCurrency     string
+	ToCurrency       string
+	FromExchangeRate string
+	ToExchangeRate   string
+}
+
+const (
+	Deposit  = "Deposit"
+	Withdraw = "Withdraw"
+	Exchange = "Exchange"
+)
